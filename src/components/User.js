4a06bec1fe4c7a884d75ea46b7f51e214c9146b7
niloychakraborty2013@gmail.com <niloychakraborty2013@gmail.com>
2020-09-05 15:30:55 +0530

    Almost Completed

--- src/components/User.js
+++ src/components/User.js
@@ -3,6 +3,8 @@ import style from './style.module.css'
 import {Link} from 'react-router-dom'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import img from './assets/profile2.png'
+
 
 const User = (props) => {
     
@@ -22,72 +24,79 @@ const User = (props) => {
     let year2 = Number(dateArray[3])
     
     return (
-        <div className={style.recipe2}  >
-    
-                <button className={style.btn}>
-                            <Link  className={style.li} to="/">
-                                  <i class="fas fa-arrow-left"></i>
-                            </Link>
-                 </button>
-                 <h3>ID : {user.id}</h3>
-                 <h3>Name : {user.real_name}</h3>
-                 <h3>TimeZone : {user.tz}</h3>
+        <div>
+                <Link  className={style.li} to="/">
+                            <i className="fas fa-arrow-left"></i>
+                </Link>
+        
+                <div className={style.recipe2}  >  
+                        
+                        <img src={img} alt={img} className={style.img2} />
+                        <div className={style.letter}>ID : {user.id}</div>
+                        <div className={style.letter}>Name : {user.real_name}</div>
+                        <div className={style.letter}>TimeZone : {user.tz}</div>
 
-                 <DatePicker selected={date} onChange={onChange}/>
+                        
 
-                 {user.activity_periods.map(activity => {
-                            let start = activity.start_time.split(' ')
-                            let end   = activity.end_time.split(' ')
-                            //Fetching the day,month and year from the start and end time of activity Array 
-                            let month = start[0]
-                            let day   = Number(start[1]) 
-                            let year  = Number(start[2])
-                            let startTime  = start[4]
-                            let endTime  = end[3]
-                            //Comparing the activiy Date with the Date which user has chosen
-                            if(day===day2 && month===month2 && year===year2){
-                                    count++
-                                    return (
-                                            <table className="table table-dark" key={start}>
-                                                <thead>
-                                                        <tr>
-                                                            <th scope="col"></th>
-                                                            <th scope="col">Date</th>
-                                                            <th scope="col">Active From</th>
-                                                            <th scope="col">Active To</th>
-                                                        </tr>
-                                                </thead>
-                                                <tbody>
-                                                        <tr>
-                                                            <th scope="row">1</th>
-                                                            <td>{`${month} ${day},${year}`}</td>
-                                                            <td>{startTime}</td>
-                                                            <td>{endTime}</td>
-                                                        </tr>
-                                                </tbody>
-                                            </table>
-                                    )   
-                            }
-                 })}
+                        <div className={style.date} >
+                            Choose Date : <DatePicker selected={date} onChange={onChange}className={style.datePicker} />
+                        </div>
+                        
 
-            
-            { count==0 ? <table className="table table-dark" >
-                                <thead>
-                                        <tr>
-                                            <th scope="col"> </th>
-                                            <th scope="col">Date</th>
-                                            <th scope="col">Activity</th>
-                                        </tr>
-                                </thead>
-                                <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>{`${month2} ${day2},${year2}`}</td>
-                                            <td>NO ACTIVITY FOUND</td>
-                                        </tr>
-                                </tbody>
-                        </table>: ''}
+                        {user.activity_periods.map(activity => {
+                                    let start = activity.start_time.split(' ')
+                                    let end   = activity.end_time.split(' ')
+                                    //Fetching the day,month and year from the start and end time of activity Array 
+                                    let month = start[0]
+                                    let day   = Number(start[1]) 
+                                    let year  = Number(start[2])
+                                    let startTime  = start[4]
+                                    let endTime  = end[3]
 
+                                    //Comparing the activiy Date with the Date which user has chosen
+                                    if(day===day2 && month===month2 && year===year2){
+                                            count++
+                                            return (
+                                                    <table className="table table-dark" key={start}>
+                                                        <thead>
+                                                                <tr>
+                                                                    <th scope="col"></th>
+                                                                    <th scope="col">Date</th>
+                                                                    <th scope="col"> From</th>
+                                                                    <th scope="col"> To</th>
+                                                                </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                                <tr>
+                                                                    <th scope="row">1</th>
+                                                                    <td>{`${month} ${day},${year}`}</td>
+                                                                    <td>{startTime}</td>
+                                                                    <td>{endTime}</td>
+                                                                </tr>
+                                                        </tbody>
+                                                    </table>
+                                            )   
+                                    }
+                        })}
+                    
+                    { count==0 ? <table className="table table-dark" >
+                                        <thead>
+                                                <tr>
+                                                    <th scope="col"> </th>
+                                                    <th scope="col">Date</th>
+                                                    <th scope="col">Activity</th>
+                                                </tr>
+                                        </thead>
+                                        <tbody>
+                                                <tr>
+                                                    <th scope="row">1</th>
+                                                    <td>{`${month2} ${day2},${year2}`}</td>
+                                                    <td>NO ACTIVITY FOUND</td>
+                                                </tr>
+                                        </tbody>
+                                </table>: ''}
+
+                </div>
         </div>
     )
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import User from './User'
 import style from './style.module.css'
 import { Link } from 'react-router-dom'
+import img from './assets/profile.jpg'
 
 const URL = 'https://my-json-server.typicode.com/niloy2019/json-server/members'
 
@@ -25,17 +26,18 @@ const UserList = () => {
 
     return (
         <div>
-            <h1>User List</h1>
+                <h1 className={style.h}>User List</h1>
                 {users.map(user => (
-                    <Link to={{ pathname:'/activity-details', state:{user : JSON.stringify(user)} }}   key={user.id} >
-                        <div className={style.recipes}>
+                    <div className={style.recipes} key={user.id}>
+                        <Link to={{ pathname:'/activity-details', state:{user : JSON.stringify(user)} }} className={style.link}>
                                 <div className={style.recipe} >
-                                        <h3>ID : {user.id}</h3>
-                                        <h3>Name : {user.real_name}</h3>
-                                        <h3>TimeZone : {user.tz}</h3>
+                                        <img src={img} alt={img} className={style.img} />
+                                        <p>ID : {user.id}</p>
+                                        <p>Name : {user.real_name}</p>
+                                        <p>TimeZone : {user.tz}</p>
                                 </div>
-                        </div>
                     </Link>
+                        </div>
                 ))}
         </div>
     )
